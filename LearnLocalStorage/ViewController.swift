ed27fb8df102da3c3f490ec9f1e1080ff816302f
Ilyasa Azmi <theazmi99ilyasa@gmail.com>
2019-07-23 10:46:36 +0700

    initial commit

--- LearnLocalStorage/ViewController.swift
+++ LearnLocalStorage/ViewController.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        let text = UserDefaults.standard.string(forKey: "name")
+//        nameLabel.text = text
+//        nameLabel.text = ""
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        var users = [User]()
+        
+        do {
+            users = try managedContext.fetch(User.fetchRequest())
+            
+            for user in users {
+                let name = user.value(forKey: "name") as! String
+                nameLabel.text! += "\(name),"
+                print(name)
+            }
+        } catch {
+            print("Error")
+        }
+        
+        
     }
 
-
+    @IBAction func save(_ sender: UIButton) {
+//        nameLabel.text = textField.text
+//        UserDefaults.standard.set(nameLabel.text, forKey: "name")
+        
+        //save using core data
+        let appDelegate = UIApplication.shared.delegate as? AppDelegate
+        
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        let user = User(context: managedContext)
+        user.name = textField.text
+        
+        do {
+            try managedContext.save()
+            print("Saved")
+        } catch {
+            print("Error")
+        }
+        
+    }
+    
+    
 }
 
